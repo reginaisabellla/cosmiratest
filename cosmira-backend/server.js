d82a95dbbed45d12ee75d61d1e91a52beecf73dd
reginaisabellla <158307469+reginaisabellla@users.noreply.github.com>
2025-02-23 15:10:58 -0500

    gina

--- cosmira-backend/server.js
+++ cosmira-backend/server.js
@@ -1,32 +1,3 @@
-
-// mongodb connection test
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://cosmiradb:cosmira25@cosmira.dktef.mongodb.net/?retryWrites=true&w=majority&appName=cosmira";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-// test end
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -36,7 +7,7 @@ app.use(cors());
 app.use(express.json()); // Allows server to handle JSON data
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/cosmira', {
+mongoose.connect('mongodb+srv://<reginaisabellla>:<cosmira25>@cosmira.dktef.mongodb.net/?retryWrites=true&w=majority&appName=cosmira', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
